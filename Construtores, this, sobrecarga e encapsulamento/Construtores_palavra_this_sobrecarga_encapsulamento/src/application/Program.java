95bf8b9ac4a002b101a04fdb193562deb187ba96
AlexCanel <Alexcanelprofissional@gmail.com>
2022-06-03 16:37:08 -0300

    Sexto commit

---
+++ Construtores, this, sobrecarga e encapsulamento/Construtores_palavra_this_sobrecarga_encapsulamento/src/application/Program.java
@@ -0,0 +1,56 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Conta;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		Conta conta;
+	
+		double deposito;
+		
+		System.out.print("Informe o numero da conta: ");
+		Integer numeroConta = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Informe nome do titular: ");
+		String name = sc.nextLine();
+		
+		System.out.print("Deseja Fazer um deposito inicial(S/N)?");
+		char depositoInicial = sc.next().charAt(0);
+		
+		if (depositoInicial == 's' || depositoInicial == 'S') {
+			System.out.print("Insira o valor do depósito inicial: ");
+			deposito = sc.nextDouble();
+			conta = new Conta(numeroConta, name);
+			conta.deposito(deposito);
+		}else {
+			conta = new Conta(numeroConta, name);
+		}
+		
+		System.out.println();
+		System.out.printf("Account data: \n"+ conta);
+		
+		System.out.println();
+		System.out.print("Enter a deposit value: ");
+		deposito = sc.nextDouble();
+		conta.deposito(deposito);
+		System.out.printf("Updated account data: \n"+ conta);
+		
+		System.out.println();
+		System.out.print("Enter a withdraw value: ");
+		double saque = sc.nextDouble();
+		conta.saque(saque);
+		System.out.printf("Updated account data: \n"+ conta);	
+		
+		sc.close();
+
+	}
+
+}
---
+++ Construtores, this, sobrecarga e encapsulamento/Construtores_palavra_this_sobrecarga_encapsulamento/src/entities/Conta.java
@@ -0,0 +1,62 @@
+package entities;
+
+public class Conta {
+	
+	private int numeroConta;
+	private String titularConta;
+	private double valorConta;
+	
+	public Conta() {	
+	}
+	
+	public Conta(int numeroConta, String titularConta, double valorConta) {
+		super();
+		this.numeroConta = numeroConta;
+		this.titularConta = titularConta;
+		this.valorConta = valorConta;
+	}
+
+	public Conta(int numeroConta, String titularConta) {
+		this.numeroConta = numeroConta;
+		this.titularConta = titularConta;
+	}
+
+	public Integer getNumeroConta() {
+		return numeroConta;
+	}
+
+	public String getTitularConta() {
+		return titularConta;
+	}
+
+	public void setTitularConta(String titularConta) {
+		this.titularConta = titularConta;
+	}
+
+	public Double getValorConta() {
+		return valorConta;
+	}
+
+	public void setValorConta(Double valorConta) {
+		this.valorConta = valorConta;
+	}
+	
+	public void deposito(double deposito) {
+		this.valorConta += deposito;
+	}
+	
+	public void saque(double saque) {
+		this.valorConta = valorConta - saque - 5.00 ;
+	}
+	
+	public String toString() {
+		return "Acount "
+			+	numeroConta
+			+	", Holder: "
+			+	titularConta
+			+	", Balance: $ "
+			+	String.format("%.2f%n", valorConta);
+				
+	}
+	
+}
